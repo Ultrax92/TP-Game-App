ef1912383605f1c6e9a314b522ec157836041d50
Mickael Guevel <127212295+Ultrax92@users.noreply.github.com>
2025-07-09 16:51:11 +0200

    add style Game.js

--- screens/Game.js
+++ screens/Game.js
@@ -2,39 +2,70 @@ import { useNavigation } from "@react-navigation/native";
 import { useEffect, useState } from "react";
 import { Alert, Button, StatusBar, StyleSheet, Text, View } from "react-native";
 export default function Game() {
-  const [choice, setChoice] = useState("");
-  const baseNumber = Math.floor(Math.random() * 100);
-  const score = Math.floor(Math.random() * 100);
+    const [choice, setChoice] = useState("");
+    const baseNumber = Math.floor(Math.random() * 100);
+    const score = Math.floor(Math.random() * 100);
 
-  const navigation = useNavigation();
-  useEffect(() => {
-    if (choice) {
-      const winner =
-        (choice === "higher" && score > baseNumber) ||
-        (choice === "lower" && baseNumber > score);
-      Alert.alert(`You've ${winner ? "won" : "lost"}`, `You scored: ${score}`);
-      navigation.goBack();
-      console.log(
-        ` base Number = ${baseNumber} et score = ${score}, le pari est ${winner}`
-      );
-    }
-  }, [baseNumber, score, choice, navigation]);
+    const navigation = useNavigation();
+    useEffect(() => {
+        if (choice) {
+            const winner =
+                (choice === "higher" && score > baseNumber) ||
+                (choice === "lower" && baseNumber > score);
+            Alert.alert(`You've ${winner ? "won" : "lost"}`, `You scored: ${score}`);
+            navigation.goBack();
+            console.log(
+                ` base Number = ${baseNumber} et score = ${score}, le pari est ${winner}`
+            );
+        }
+    }, [baseNumber, score, choice, navigation]);
 
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      <Text>Starting: {baseNumber}</Text>
-      <Button onPress={() => setChoice("higher")} title="Higher" />
-      <Button onPress={() => setChoice("lower")} title="Lower" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <StatusBar barStyle="dark-content" />
+            <Text style={baseNumber}>Starting: {baseNumber}</Text>
+            <TouchableHighlight onPress={() => setChoice('higher')} style={[styles.button, styles.buttonGreen]}>
+                <Text style={styles.buttonText}>Higher</Text>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={() => setChoice('lower')} style={[styles.button, styles.buttonRed]}>
+                <Text style={styles.buttonText}>Higher</Text>
+            </TouchableHighlight>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ffffff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#ffffff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    baseNumber: {
+        fontSize: 48,
+        marginBottom: 30,
+    },
+
+    button: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 15,
+        padding: 30,
+        marginVertical: 15,
+    },
+
+    buttonRed: {
+        backgroundColor: 'red',
+    },
+
+    buttonGreen: {
+        backgroundColor: 'green',
+    },
+
+    buttonText: {
+        color: 'white',
+        fontSize: 24,
+    },
 });
--- screens/Home.js
+++ screens/Home.js
@@ -1,45 +1,45 @@
 import { useNavigation } from "@react-navigation/native";
 import {
-  StatusBar,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
+    StatusBar,
+    StyleSheet,
+    Text,
+    TouchableHighlight,
+    View,
 } from "react-native";
 export default function Home() {
-  const navigation = useNavigation();
+    const navigation = useNavigation();
 
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle={"dark-content"} />
-      <TouchableHighlight
-        onPress={() => navigation.navigate("Game")}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Start game !</Text>
-      </TouchableHighlight>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <StatusBar barStyle={"dark-content"} />
+            <TouchableHighlight
+                onPress={() => navigation.navigate("Game")}
+                style={styles.button}
+            >
+                <Text style={styles.buttonText}>Start game !</Text>
+            </TouchableHighlight>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ffffff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  button: {
-    width: 300,
-    height: 300,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-around",
-    borderRadius: 150,
-    backgroundColor: "orange",
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 48,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#ffffff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    button: {
+        width: 300,
+        height: 300,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-around",
+        borderRadius: 150,
+        backgroundColor: "orange",
+    },
+    buttonText: {
+        color: "white",
+        fontSize: 48,
+    },
 });
