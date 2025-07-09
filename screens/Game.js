0cebb1c9b91087ab7f7f0f36434545a5446460f2
Thomas <boy1.thoomas@gmail.com>
2025-07-09 15:42:39 +0200

    change game.js

--- screens/Game.js
+++ screens/Game.js
@@ -1,38 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, StatusBar, Button, Alert } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
+import { useEffect, useState } from "react";
+import { Alert, Button, StatusBar, StyleSheet, Text, View } from "react-native";
 export default function Game() {
-    const [choice, setChoice] = useState('');
-    const baseNumber = Math.floor(Math.random() * 100);
-    const score = Math.floor(Math.random() * 100);
+  const [choice, setChoice] = useState("");
+  const baseNumber = Math.floor(Math.random() * 100);
+  const score = Math.floor(Math.random() * 100);
 
-    const navigation = useNavigation();
-    useEffect(() => {
-        if (choice) {
-            console.log(` base Number = ${baseNumber} et score = ${score}`);
-            const winner =
-                (choice === 'higher' && score > baseNumber) ||
-                (choice === 'lower' && baseNumber > score);
-            Alert.alert(`You've ${winner ? 'won' : 'lost'}`, `You scored: ${score}`);
-            navigation.goBack();
-        }
-    }, [baseNumber, score, choice])
+  const navigation = useNavigation();
+  useEffect(() => {
+    if (choice) {
+      const winner =
+        (choice === "higher" && score > baseNumber) ||
+        (choice === "lower" && baseNumber > score);
+      Alert.alert(`You've ${winner ? "won" : "lost"}`, `You scored: ${score}`);
+      navigation.goBack();
+      console.log(
+        ` base Number = ${baseNumber} et score = ${score}, le pari est ${winner}`
+      );
+    }
+  }, [baseNumber, score, choice]);
 
-    return (
-        <View style={styles.container}>
-            <StatusBar barStyle="dark-content" />
-            <Text>Starting: {baseNumber}</Text>
-            <Button onPress={() => setChoice('higher')} title="Higher" />
-            <Button onPress={() => setChoice('lower')} title="Lower" />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <Text>Starting: {baseNumber}</Text>
+      <Button onPress={() => setChoice("higher")} title="Higher" />
+      <Button onPress={() => setChoice("lower")} title="Lower" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#ffffff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
